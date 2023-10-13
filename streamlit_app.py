558aef841f88be6f942fa8de85788de9ea884901
mireyalbl <131256112+mireyalbl@users.noreply.github.com>
2023-10-13 10:42:26 +0200

    Update streamlit_app.py
    fixed text

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,8 @@ import streamlit
 
 streamlit.title('My parents New Healthy Diner')
 
-streamlit.header('🥣 Breakfast Favorites')
-streamlit.text('🥗 Omega 3 & Blueberry Oatmeal')
-streamlit.text('🐔 Kale, Spinach & Rocket Smoothie')
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
